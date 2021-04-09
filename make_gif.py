98d9a2805a4fb97c1d1ce13f80f8d7b4ead47faf
AS04 <48136120+MrAS04@users.noreply.github.com>
2021-04-09 17:27:05 +0400

    Removed extraneous stuff from the python scripts

--- make_gif.py
+++ make_gif.py
@@ -1,27 +1,7 @@
-import os
-import glob
-import re
-import shutil
-
-# try:
-#     import ffmpeg
-# except ImportError:
-#     print("ffmpeg-python bindings not installed. Installing...")
-#     os.system("pip3 install ffmpeg-python")
-
-import ffmpeg
-
-# try:
-#     from PIL import Image
-# except ImportError:
-#     print("Pillow not installed. Installing...")
-#     os.system("pip3 install Pillow")
-
+import os, glob, re, shutil, ffmpeg, sys
 from PIL import Image
 
-import sys
-
-png_dir = os.path.join(os.getcwd(), "cropped_images", "img%d.bmp")
+img_dir = os.path.join(os.getcwd(), "cropped_images", "img%d.bmp")
 
 # Get dimensions.
 im = Image.open(os.path.join(os.getcwd(), "cropped_images", "img0.bmp"))
@@ -34,11 +14,11 @@ else:
 if not name:
     name = "output.mp4"
 
-print("Making mp4 / whatever using FFMPEG...")
+#print("Making mp4 / whatever using FFMPEG...")
 
 (
     ffmpeg
-    .input(png_dir, pattern_type='sequence', framerate=30)
+    .input(img_dir, pattern_type='sequence', framerate=30)
     .output(name, r=30, **{'filter:v': f"scale='min({width},iw)':min'({height},ih)':force_original_aspect_ratio=decrease,pad={width}:{height}:(ow-iw)/2:(oh-ih)/2"}, **{'frames:v': width})
     .run()
 )
--- png2bin.py
+++ png2bin.py
@@ -2,28 +2,6 @@ from PIL import Image
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-
-def write_image(image, filename):
-    height = image.height
-    width = image.width
-
-    f = open(filename, "wb")
-
-    f.write(height.to_bytes(2, byteorder='big'))
-    f.write(width.to_bytes(2, byteorder='big'))
-    img_raster = []
-    for i in range(height):
-        for j in range(width):
-            img_raster.extend(image.getpixel((j, i))[:3])
-
-    f.write(bytearray(img_raster))
-    f.close()
-
-def read_2bytes(f):
-    bytes = f.read(2) # [int(f.read(1)), int(f.read(1))]
-    return int.from_bytes(bytes, byteorder = 'big')
-
-
 def read_image(filename):
     f = open(filename, "rb")
     height = read_2bytes(f)
@@ -38,20 +16,6 @@ def read_image(filename):
 
     return image
 
-#Write a png image to bin
 img = input("Please give the path to the image (include file extension): ")
 image = Image.open(img)
 write_image(image, f"{img.split('.')[0]}.bin")
-# image = Image.open("img/3x4.png")
-# write_image(image, "img/3x4.bin")
-#
-# Read image from a bin file, save it to png
-# im2 = read_image("a.bin")
-# im3 = read_image("6x5_grad.bin")
-# im3.save("grad.png")
-
-
-# Write multiple images from bin to png
-# for i in range(200):
-#     image = read_image("img%d.bin" % i)
-#     image.save("img%d.png" % i)
